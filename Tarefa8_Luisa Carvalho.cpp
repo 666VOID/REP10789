9794c44a4863a7f96960571aaafb695de32764b4
666VOID <luisaefcarvalho@gmail.com>
2023-10-16 12:38:58 +0100

    Programa atualizado

--- Tarefa8_Luisa Carvalho.cpp
+++ Tarefa8_Luisa Carvalho.cpp
@@ -4,11 +4,9 @@
 using namespace std;
 
 int main() {
-	
-	setlocale(LC_ALL, "Portuguese");
-		   
+    setlocale(LC_ALL, "Portuguese");
+    
     double nota1, nota2, nota3, nota4;
-
 
     cout << "Primeira nota:" << endl;
     cin >> nota1;
@@ -23,10 +21,10 @@ int main() {
     cin >> nota4;
 
  
-    double media = (nota1 + nota2 + nota3 + nota4) / 4;
+    double media = (nota1 * 0.5) + (nota2 * 0.1) + (nota3 * 0.1) + (nota4 * 0.3);
 
     cout << "Notas inseridas: " << nota1 << ", " << nota2 << ", " << nota3 << ", " << nota4 << endl;
-    cout << "Média das notas: " << media << endl;
+    cout << "Média ponderada das notas: " << media << endl;
 
     return 0;
 }
